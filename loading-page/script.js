4741dd2eeca4092e89dffbd82c18a606b4e99287
Ibad laskar <ibalaskar6@gmail.com>
2023-10-05 09:08:40 +0530

    Create script.js

---
+++ loading-page/script.js
@@ -0,0 +1,24 @@
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 3.5,
+  }
+);
+
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.5,
+  }
+);
