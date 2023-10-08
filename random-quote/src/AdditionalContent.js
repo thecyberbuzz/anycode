25f69693e987ee9a560ec17da8fb456bccda8bd4
Avishka Indula <avishkainduladev@gmail.com>
2023-10-08 12:33:47 +0530

    Created RandomQuotes Site

---
+++ random-quote/src/AdditionalContent.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AdditionalContent = () => {
+  return (
+    <div style={{ marginTop: "20px", marginLeft: "100px", marginRight: "100px" }}>
+      <p>
+        Embrace the wisdom of each quote and let it guide your journey. Life is
+        a collection of moments, and within these words, find inspiration to
+        overcome challenges, embrace joy, and make a positive impact. As you
+        reflect on the profound thoughts expressed, consider how they resonate
+        with your own experiences and aspirations. Remember, each day is an
+        opportunity to learn, grow, and contribute to the beautiful tapestry of
+        existence.
+      </p>
+    </div>
+  );
+};
+
+export default AdditionalContent;
