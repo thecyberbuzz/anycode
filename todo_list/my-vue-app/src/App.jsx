b81a25fd92215899b4b9af44324ea6ed457eb493
prashanta <prashantachowdhury683@gmail.com>
2023-10-11 19:05:37 +0530

    1st commit done

---
+++ todo_list/my-vue-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Todo from './Components/Todoreact/Todo';
+import './App.css'
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
