0144a11e95a64fb7030f78147a620191422781e0
lucashchang <146160465+lucashchang@users.noreply.github.com>
2023-10-23 20:45:56 -0700

    Create Calorie Calculator .py
    calorie calculator

---
+++ Calorie Calculator .py
@@ -0,0 +1,13 @@
+calories_ttl = 0
+running = True
+while running:
+  add = input("What's one food ate eat today? ")
+  calories = int(input("how many calories was that? ")
+  calories_ttl += calories
+  finished = input("Is this all you ate today? Yes or No.")
+  if finished == "yes" or "Yes":
+    running = False
+  else:
+    running = True
+
+print("You ate " + calories_ttl + " calories today!")
